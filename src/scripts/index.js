2345ac56937399d4dc5f0c19be1877c099dfc43a
Johan R <johanreza58@gmail.com>
2023-04-13 17:07:26 +0700

    responsive

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,21 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
+import "../styles/responsive.css";
+
+const navToggle = document.querySelector(".nav-title__toggle");
+const links = document.querySelector(".nav-item");
+const main = document.querySelector("main");
+const hero = document.querySelector(".hero-image");
+
+navToggle.addEventListener("click", function (event) {
+  links.classList.toggle("show_nav");
+  event.stopPropagation();
+});
+
+main.addEventListener("click", function () {
+  links.classList.remove("show_nav");
+});
+
+hero.addEventListener("click", function () {
+  links.classList.remove("show_nav");
+});
