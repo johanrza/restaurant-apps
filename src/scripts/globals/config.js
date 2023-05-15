52524038d7c01d324a876e272df2077daa6df4d2
Johan R <johanreza58@gmail.com>
2023-05-15 11:40:33 +0700

    add sw and implement cache API

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,6 +2,8 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL_SMALL: 'https://restaurant-api.dicoding.dev/images/small/',
   BASE_IMAGE_URL_LARGE: 'https://restaurant-api.dicoding.dev/images/large/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'RestaurantsCatalogue-V1',
 };
 
 export default CONFIG;
